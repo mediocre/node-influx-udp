001c24863f985d03fd08cebace2c9acc84f62ecc
Toby Brain <tobio85@gmail.com>
2016-09-01 21:13:17 -0700

    Format string values correctly

--- test/index.js
+++ test/index.js
@@ -27,11 +27,39 @@ test('should send a single data point via UDP', function(done) {
 
     assert.equal(
         scope.buffer.toString(),
-        "metric1,tkey1=tval1,tkey2=tval2 fkey=fval,fkey2=fval2 1468430065292"
+        "metric1,tkey1=tval1,tkey2=tval2 fkey=\"fval\",fkey2=\"fval2\" 1468430065292"
     );
     scope.done();
     done();
 });
+
+test('should properly format typed values', function(done) {
+   var influx_udp = new InfluxUdp({
+     host: 'example.com',
+     port: '8086',
+     protocol: 'line',
+     reportTime: true,
+   });
+
+    var scope = mock_udp('example.com:8086');
+
+    influx_udp.send({
+        'metric1': [
+            {
+                values: { fkey: "string_val", fkey2: true, fkey3: 12345 },
+                tags: { tkey1: "tval1", tkey2: "tval2" },
+                time: 1468430065292
+            }
+        ]
+    });
+
+    assert.equal(
+        scope.buffer.toString(),
+        "metric1,tkey1=tval1,tkey2=tval2 fkey=\"string_val\",fkey2=true,fkey3=12345 1468430065292"
+    );
+    scope.done();
+    done();
+})
 
 test('should send a single data point via UDP-json', function(done) {
    var influx_udp = new InfluxUdp({
@@ -62,7 +90,7 @@ test('should send a single data point via UDP-json', function(done) {
                 [
                     {"fkey": "fval", "fkey2": "fval2"},
                     {"tkey1": "tval1", "tkey2": "tval2"},
-                    1468430065292 
+                    1468430065292
                 ]
             ]
         }]
